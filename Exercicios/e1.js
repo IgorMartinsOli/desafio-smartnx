9b3cf3b68b8dd0262fe8e8f1bd8eff5daa909f95
Igor_ <igrmartoli@gmail.com>
2022-07-30 17:44:58 -0300

    e1 e e2

---
+++ Exercicios/e1.js
@@ -0,0 +1,8 @@
+
+//E.1 Crie uma função que recebe dois argumentos string e retorna o de maior comprimento.
+let maiorString = (string1, string2) => {
+    return string1.length > string2.length ? string1 : string2;
+}
+
+console.log(maiorString("carro", "moto"));
+console.log(maiorString("carro", "motoss"));
---
+++ Exercicios/e2.js
@@ -0,0 +1,12 @@
+//E.1 Crie uma função que recebe dois argumentos string e retorna o de maior comprimento.
+function maiorString(string1, string2) {
+    return string1.length > string2.length ? string1 : string2;
+}
+
+// E.2 Crie uma função que recebe três argumentos, uma função e duas string, aplique a função nas duas string e imprima o resultado.
+function imprimeMaiorString(maiorString, string1, string2) {
+    console.log(maiorString(string1, string2));
+}
+
+imprimeMaiorString(maiorString, 'hello', 'worldss');
+imprimeMaiorString(maiorString, 'helloss', 'world');
